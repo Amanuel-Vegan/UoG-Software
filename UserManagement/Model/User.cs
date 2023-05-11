1c3f16d5e9b693ef2dbc7e79e3f38f102b1a4e13
Amanuel <amanuel.eshete1@gmail.com>
2023-05-11 23:30:45 +0300

    #1 New clean .NET Core API solution Created and configered Db Context, Connection String is complated.

---
+++ UserManagement/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace UserManagement.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+         
+        public User()
+        {
+          Id = Guid.NewGuid();
+        }
+    }
+}
+
